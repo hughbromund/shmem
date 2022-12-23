cce9525b224d5345e0206499c2f87d6a96cfe4db
Hugh Bromund <hughbromund@gmail.com>
2022-12-23 13:24:57 -0500

    More work on header design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,18 +3,72 @@ import { ReactComponent as Logo } from "../../assets/Logo.svg";
 
 import classes from "./Header.module.css";
 
+var classNames = require("classnames");
+
+enum Project {
+  DubbClub = "DubbClub",
+  Finex = "Finex",
+}
+
 type Props = {};
 
-type State = {};
+type State = {
+  currentProject: Project | null;
+};
 
 export default class Header extends Component<Props, State> {
-  state = {};
+  state = {
+    currentProject: null,
+  };
 
   render() {
     return (
       <div className={classes.wrapper}>
-        <Logo className={classes.Logo} />
-        SHMEM.io
+        <div className={classes.inner}>
+          <Logo className={classes.Logo} />
+        </div>
+        <div className={classes.container}>
+          <span
+            className={classNames(classes.project, {
+              [classes.visible]: this.state.currentProject === Project.DubbClub,
+            })}
+          >
+            DubbClub.
+          </span>
+
+          {/* <span
+            className={classNames(classes.project, {
+              [classes.visible]: this.state.currentProject === Project.Finex,
+            })}
+          >
+            finex.
+          </span> */}
+
+          <span>
+            <strong>shmem.io</strong>
+          </span>
+        </div>
+        <button
+          onClick={() => {
+            this.setState({ currentProject: Project.DubbClub });
+          }}
+        >
+          Dubb Club
+        </button>
+        <button
+          onClick={() => {
+            this.setState({ currentProject: Project.Finex });
+          }}
+        >
+          FINEX
+        </button>
+        <button
+          onClick={() => {
+            this.setState({ currentProject: null });
+          }}
+        >
+          Clear
+        </button>
       </div>
     );
   }
